ff2ed44a5c67c7c63179d651aa91ec20bb99620a
THANHTHAO <phanthanhthao.2704@gmail.com>
2022-04-25 21:23:54 +0700

    commit bt

---
+++ BAi_KIEM_TRA/BAi_KIEM_TRA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BAi_KIEM_TRA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
