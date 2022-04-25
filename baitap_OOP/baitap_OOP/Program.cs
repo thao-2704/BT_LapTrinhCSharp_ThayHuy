bb8f835e4861cd5a2e28d18ab607c646d92335f2
THANHTHAO <phanthanhthao.2704@gmail.com>
2022-04-25 20:59:23 +0700

    bt

---
+++ baitap_OOP/baitap_OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace baitap_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
