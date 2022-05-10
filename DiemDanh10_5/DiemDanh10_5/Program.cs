0115053cada65bb4086a1bd5def2d52baaec55f5
THANHTHAO <phanthanhthao.2704@gmail.com>
2022-05-10 08:09:08 +0700

    diemdanh

--- DiemDanh10_5/DiemDanh10_5/Program.cs
+++ DiemDanh10_5/DiemDanh10_5/Program.cs
@@ -4,46 +4,87 @@ namespace DiemDanh10_5
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int[] arr1 = new int[100];
-            int i, mx, mn, n;
-
-
-            Console.Write("\nTim phan tu lon nhat, phan tu nho nhat trong mang trong C#:\n");
-            Console.Write("-----------------------------------------------------------\n");
-
-            Console.Write("Nhap so phan tu can luu giu trong mang: ");
-            n = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Nhap {0} phan tu vao trong mang:\n", n);
-            for (i = 0; i < n; i++)
+        static void Nhap_mang(int[] a_258, int n)
+        {  
+            for (int i = 0; i < n; i++)
             {
-                Console.Write("Phan tu - {0}: ", i);
-                arr1[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Phan tu  a[{0}]: ", i);
+                a_258[i] = int.Parse(Console.ReadLine());
             }
-
-
-            mx = arr1[0];
-            mn = arr1[0];
+        }
+        static void Xuat_mang(int[] a_258, int n)
+        {
+            Console.Write("Mang vua nhap: ");
+            for( int i=0; i<n; i++)
+            {
+                Console.Write(a_258[i] + " ");
+            }
+        }
+        static void Max_min(int[] a_258, int n)
+        {
+            Console.Write("-----------------------------------------------------------\n");
+            Console.Write("\nTim phan tu lon nhat, phan tu nho nhat trong mang:\n");
+            
+            int i, max_258, min_258;
+            max_258 = a_258[0];
+            min_258 = a_258[0];
 
             for (i = 1; i < n; i++)
             {
-                if (arr1[i] > mx)
+                if (a_258[i] > max_258)
                 {
-                    mx = arr1[i];
+                    max_258 = a_258[i];
                 }
 
 
-                if (arr1[i] < mn)
+                if (a_258[i] < min_258)
                 {
-                    mn = arr1[i];
+                    min_258 = a_258[i];
                 }
             }
-            Console.Write("Phan tu lon nhat trong mang la: {0}\n", mx);
-            Console.Write("Phan tu nho nhat trong mang la: {0}\n\n", mn);
+            Console.Write("Phan tu lon nhat trong mang la: {0}\n", max_258);
+            Console.Write("Phan tu nho nhat trong mang la: {0}\n\n", min_258);
+        }
+        static void Sap_Xep(int[] a_258, int n)
+        {
+            int tg_258;
+            for (int i = 0; i < n; i++)
+            {
+                for ( int j = i + 1; j < n; j++)
+                {
+                    if (a_258[j] < a_258[i])
+                    {
+                        //cach trao doi gia tri
+                        tg_258 = a_258[i];
+                        a_258[i] = a_258[j];
+                        a_258[j] = tg_258;
+                    }
+                }
+            }
+            Console.Write("-----------------------------------------------------------\n");
+            Console.Write("\nCac phan tu mang theo thu tu tang dan:\n");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}  ", a_258[i]);
+            }
+
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] arr_258 = new int[100];
+            int i, n;
+            Console.Write("Nhap so phan tu : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Nhap_mang(arr_258,n);
+            Xuat_mang(arr_258, n);
+            Max_min(arr_258, n);
+            Sap_Xep(arr_258, n);
+
 
             Console.ReadKey();
         }
+
     }
+    
 }
