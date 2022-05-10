78896dc340a57dadd8c7686916ba7852bc9a0137
THANHTHAO <phanthanhthao.2704@gmail.com>
2022-05-10 07:28:43 +0700

    diem danh 10_5

---
+++ DiemDanh10_5/DiemDanh10_5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DiemDanh10_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[100];
+            int i, mx, mn, n;
+
+
+            Console.Write("\nTim phan tu lon nhat, phan tu nho nhat trong mang trong C#:\n");
+            Console.Write("-----------------------------------------------------------\n");
+
+            Console.Write("Nhap so phan tu can luu giu trong mang: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nhap {0} phan tu vao trong mang:\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Phan tu - {0}: ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            mx = arr1[0];
+            mn = arr1[0];
+
+            for (i = 1; i < n; i++)
+            {
+                if (arr1[i] > mx)
+                {
+                    mx = arr1[i];
+                }
+
+
+                if (arr1[i] < mn)
+                {
+                    mn = arr1[i];
+                }
+            }
+            Console.Write("Phan tu lon nhat trong mang la: {0}\n", mx);
+            Console.Write("Phan tu nho nhat trong mang la: {0}\n\n", mn);
+
+            Console.ReadKey();
+        }
+    }
+}
